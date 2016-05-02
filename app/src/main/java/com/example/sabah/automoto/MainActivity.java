72efc591dd7f6391f5884f7ebc6170a1c465c6ab
skhaldi <sabah@voxalto.com>
2016-05-02 18:08:15 +0100

    updating marging

--- app/src/main/java/com/example/sabah/automoto/MainActivity.java
+++ app/src/main/java/com/example/sabah/automoto/MainActivity.java
@@ -26,14 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnInfoMnt.setOnClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     public void onClick(View view) {
